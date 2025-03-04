5609b564d25486cf36daee7b87dae9dcd9811ad9
ryansmislang <ryansmislang@gmail.com>
2025-03-04 19:05:15 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import AppNavbar from "./components/AppNavbar";
+
+ 
+import Home from "./pages/Home";
+import Products from "./pages/Workouts";
+
+import AddProduct from "./components/AddWorkout";
+import Register from "./pages/Register";
+import Login from './pages/Login';
+import Logout from "./pages/Logout";
+
+import { UserProvider } from "./context/UserContext";
+
 
 function App() {
+  
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <UserProvider value={{user, setUser, unsetUser}}>
+        <Router>
+          <AppNavbar/>
+          <Container>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/workouts" element={<Workouts />} />
+              
+              <Route path="/addWorkout" element={<AddWorkout />} />
+              
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/logout" element={<Logout />} />
+             
+
+          </Routes>
+
+          </Container>
+           <Footer />
+        </Router>
+
+      </UserProvider>
+    </>
+
+  )
 }
 
 export default App;
---
+++ src/components/AppNavbar.js
@@ -0,0 +1,45 @@
+import { useState, useContext } from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from "react-router-dom";
+import UserContext from "../context/UserContext";
+
+
+export default function AppNavbar(){
+
+
+	const { user } = useContext(UserContext);
+
+	console.log(user);
+
+	return (
+		<Navbar className="custom-navbar" expand="lg">
+		    <Container>
+		        <Navbar.Brand as={NavLink} to="/">Grit & Co.</Navbar.Brand>
+		        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+		        <Navbar.Collapse id="basic-navbar-nav">
+		            <Nav className="ms-auto"> 
+		                <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+		                
+		                {user.id !== null ? (
+		                       
+		                        <>   
+		                        	<Nav.Link as={NavLink} to="/products">Workouts</Nav.Link>
+		                            <Nav.Link as={NavLink} to="/logout">Add Workout</Nav.Link>
+		                            <Nav.Link as={NavLink} to="/logout">Logout</Nav.Link>
+		                        </>
+		                    
+		                ) : (
+		                    <>
+		                        <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+		                        <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
+		                    </>
+		                )}
+		            </Nav>
+		        </Navbar.Collapse>
+		    </Container>
+		</Navbar>
+
+	)
+}
